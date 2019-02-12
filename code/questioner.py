5b9d1e3d5962a42a13af2ee2ca62653227b80162
Rik <rbose@cs.rochester.edu>
2019-02-12 16:25:40 -0500

    a basic harness essentially in place.  Need to put it into a loop and make the inspection prettier

--- code/questioner.py
+++ code/questioner.py
@@ -1,7 +1,9 @@
-import logging
-from tripsweb.query import get_parse
+import logging, os
+from tripsweb import query
 import json
 
+from TripsIM import matcher
+from pprint import pprint
 
 logging.basicConfig()
 logger = logging.getLogger()
@@ -10,7 +12,31 @@ TRIPS_URL="http://trips.ihmc.us/parser/cgi/step"
 def get_parse(sentence):
     cfg = json.load(open("data/parserconfig.json"))
     cfg['input'] = sentence
-    parse, code = get_parse(TRIPS_URL, cfg, as_xml=False, verbose=False)
+    parse, code = query.get_parse(TRIPS_URL, cfg, as_xml=False, verbose=False)
     return json.loads(parse)
 
+def load_rulesets(directory):
+    rulesets = {}
+    # use os.walk here instead for better organization of rule sets
+    for filename in os.listdir(directory):
+        if filename.endswith(".im"):
+            rulesets[filename[0:-3]] = matcher.parse_rule_set(os.path.join(directory, filename))
+    return rulesets
 
+def example(sentence, rulesets):
+    sentence = get_parse(sentence)
+    pprint(sentence)
+    s = matcher.load_list_set(sentence)
+    results = {}
+    for name, ruleset in rulesets.items():
+        logger.info("matching {name}".format(name=name))
+        results["name"] = matcher.grade_rules(ruleset, s)
+    return sentence, results
+
+def main():
+    rulesets = load_rulesets("data/rulesets")
+    results = example("Shall I buy a computer?", rulesets)
+    return results
+
+if __name__ == "__main__":
+    main()
